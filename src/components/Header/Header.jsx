f7d50e8b38c14b6d6bf4fad833ff371df9179df5
Haka <lalka7958@gmail.com>
2025-05-03 15:26:46 +0300

    Added logic to the search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,26 +8,35 @@ import { ROUTES } from "../../utils/routes";
 import LOGO from "../../images/logo.svg";
 import AVATAR from "../../images/avatar.jpg";
 
-import { toggleForm } from '../../features/user/userSlice'
+import { toggleForm } from "../../features/user/userSlice";
+import { useGetProductsQuery } from "../../features/api/apiSlice";
 
 const Header = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { currentUser } = useSelector(({ user }) => user)
+  const [searchValue, setSearchValue] = useState("");
 
-  const [values, setValues] = useState({ name: 'Guest', avatar: AVATAR})
-  
+  const { currentUser } = useSelector(({ user }) => user);
+
+  const [values, setValues] = useState({ name: "Guest", avatar: AVATAR });
+
+  const { data, isLoading } = useGetProductsQuery({ title: searchValue });
+
   useEffect(() => {
-    if(!currentUser) return;
-    
-    setValues(currentUser)
-  }, [currentUser])
-  
+    if (!currentUser) return;
+
+    setValues(currentUser);
+  }, [currentUser]);
+
   const handleClick = () => {
-    if(!currentUser) dispatch(toggleForm(true))
+    if (!currentUser) dispatch(toggleForm(true));
     else navigate(ROUTES.PROFILE);
-  }
+  };
+
+  const handleSearch = ({ target: { value } }) => {
+    setSearchValue(value);
+  };
 
   return (
     <div className={styles.header}>
@@ -56,23 +65,46 @@ const Header = () => {
               name="search"
               placeholder="Seatch for anyting..."
               autoComplete="off"
-              onChange={() => {}}
-              value=''
+              onChange={handleSearch}
+              value={searchValue}
             />
           </div>
 
-          {false && <div className={styles.box}></div> }
+          {searchValue && (
+            <div className={styles.box}>
+              {isLoading
+                ? "Loading"
+                : !data.length
+                ? "No results"
+                : data.map(({ title, images, id }) => {
+                    return (
+                      <Link
+                        key={id}
+                        onClick={() => setSearchValue("")}
+                        className={styles.item}
+                        to={`/products/${id}`}
+                      >
+                        <div
+                          className={styles.image}
+                          style={{ backgroundImage: `url(${images[0]})` }}
+                        />
+                        <div className={styles.title}>{title}</div>
+                      </Link>
+                    );
+                  })}
+            </div>
+          )}
         </form>
 
         <div className={styles.account}>
           <Link to={ROUTES.HOME} className={styles.favourites}>
-            <svg className={styles['icon-fav']}>
+            <svg className={styles["icon-fav"]}>
               <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`} />
             </svg>
           </Link>
 
           <Link to={ROUTES.CART} className={styles.cart}>
-            <svg className={styles['icon-cart']}>
+            <svg className={styles["icon-cart"]}>
               <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`} />
             </svg>
             <span className={styles.count}>2</span>
